e37b72d5affc9fc58b61e72f3169b1ede6898a48
Przemyslaw Zalewski <przemyslawwys@wp.pl>
2018-02-08 14:42:40 +0100

    Add half tree in js code

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,9 @@
-var text = 'Velociraptor is a genus of herbivorous ceratopsid dinosaur that first appeared during the late Maastrichtian stage of the late Cretaceous period.';
-
-var dinosaur = 'triceratops';
-
-var dinosaurUpperCased = dinosaur.toUpperCase();
-
-var textReplaced = text.replace('Velociraptor', dinosaurUpperCased);
-
-var half = Math.ceil(textReplaced.length/2);
-
-var firstHalf = textReplaced.slice(0, half);
-
-console.log(firstHalf);
+function drawTree (heightTree) {
+	for (var i = 1; i <= heightTree; i++) {
+		star = '';
+		for (var j = 1; j <= i; j++) {
+			star += '*';
+        }
+		console.log(star)
+    }
+} 
